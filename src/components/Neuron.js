02be79a53d65e4040aad11077d4edb2d8790e406
joeyhuaa <joeyhua17@gmail.com>
2020-04-16 18:30:41 -0700

    circles responsive

--- src/components/Neuron.js
+++ src/components/Neuron.js
@@ -1,39 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Neuron(props) {
+
   let circleStyles = {
-    'border':'solid black 2px',
-    'borderRadius':'50%',
-    'width':'130px',
-    'height':'130px',
-    'backgroundColor':'#A1D363',
     'position':'absolute',
     'left':props.pos.x,
     'top':props.pos.y,
-    'display':'flex',
-    'alignItems':'center',
-    'justifyContent':'center'
-  }
-
-  let textAreaStyles = {
     'margin':'0',
     'padding':'0',
-    'backgroundColor':'transparent',
-    'height':'100%',
-    'width':'100%',
-    'border':'none',
+    'backgroundColor':'#A1D363',
+    'height':'130px',
+    'width':'130px',
+    'border':'solid black 2px',
     'borderRadius':'50%',
     'fontSize':'20px',
     'outline':'none',
     'resize':'none',  
     'textAlign':'center',
     'verticalAlign':'middle',
+    'cursor':'pointer',
   }
+
+  let circleSelectedStyles = {...circleStyles, 'border':'solid pink 6px'}
+
+  /* HOOKS */
+  let [hovering, setHovering] = useState(false)
+
+  useEffect(() => {
+    console.log(hovering)
+  })
+
+  let handleHover = () => {
+    setHovering(!hovering)
+  }
+
+  /* TEST */
+  let testStyles = {
+    'position':'absolute', 'left':props.pos.x, 'top':props.pos.y, 'border':'solid green 3px'
+  }
+
+  let testHoverStyles = {...testStyles, 'border':'solid blue 3px'}
 
   // this circle will be rendered whenever a mouse click event occurs in Brain
   return (
-    <div style={circleStyles}>
-      <div className='max-lines' contentEditable='true' style={textAreaStyles}></div>
+    // <div style={hovering ? circleSelectedStyles : circleStyles} onMouseEnter={handleHover} onMouseOut={handleHover}>
+    //   <div className='max-lines' contentEditable='true' style={textAreaStyles}></div>
+    // </div>
+    <div contentEditable>
+      {/* <svg height='150' width='150' style={hovering ? circleSelectedStyles : circleStyles}>
+      </svg> */}
+      <textarea style={hovering ? circleSelectedStyles : circleStyles} onMouseEnter={handleHover} onMouseOut={handleHover}></textarea>
     </div>
   )
 }
